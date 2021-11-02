342cf3efe08d3e162dd7849ba1cf24d60e49cafc
Vanshika <vanshikag0111@gmail.com>
2021-11-02 13:33:13 +0530

    cleared the loc

--- Stacks/peek.cpp
+++ Stacks/peek.cpp
@@ -15,38 +15,6 @@ using namespace std;
 
 int main()
 {
-    int MAX=10, stack[MAX], top, i, size;
-
-    cout << "Enter size of the stack: ";
-    cin >> size;
-    cout << endl;
-
-    cout << "Enter elements of the stack: " << endl;
-    for(i=0; i<size; i++)
-    {
-        cout << "Element " << i+1 << ": " ;
-        cin >> stack[i];
-    }
-
-    cout << "Displaying stack: " << endl;
-    for(i=0; i<size; i++)
-    {
-        cout << stack[i] << endl;
-    }
-
-    cout << "Peek" << endl;
-    if(top == -1)
-    {
-        cout << "Stack underflow" << endl;
-        return -1;
-    }
-    else
-    {
-        // cout << "The top-most element of the stack is " << stack[top] << endl;
-        return stack[top];
-        
-    }
-
 
     return 0;
 }
