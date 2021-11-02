6a5176e1173480b97f0e7feb3cdf68f812cc44e4
Vanshika <vanshikag0111@gmail.com>
2021-11-02 13:13:23 +0530

    push operation of stack completed

--- Stacks/push.cpp
+++ Stacks/push.cpp
@@ -8,79 +8,57 @@ Insertion:
     Else, it give rise to an error of "OVERFLOW".
 */
 
-#include<iostream>
-#include<stdlib.h>
-using namespace std;
 
-// CASE 1: Stack size is taken from the user
+#include<iostream>
+using namespace std;
+#define MAX 5
+
+// global declaration
+int stack[MAX] = {1,2,3,4}, top=3, i, value;        // --> for insertion case
+// int stack[MAX] = {1,2,3,4,5}, top=4, i, value;   // --> for overflow case
+
+// function prototype
+int push(int stack[], int value);
+void display(void);
+
 int main()
 {
-    int stack[10], top=-1, MAX=10, new_element;
-    int size, i;
-
-    printf("Enter the size of stack: ");
-    scanf("%d", &size);
-    printf("\n ------------- \n");
-
-    printf("Enter elements of stack: \n");
-    for(i=0; i<size; i++)
-    {
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &stack[i]);
-    }
-
-    printf("\n ------------ \n");
-    cout << "Enter the number to be inserted in the stack: ";
-    cin >> new_element;
-    cout << endl;
-
-    cout << "PUSHING ~ INSERTION" << endl;
-    if (top == MAX-1)
-    {
-        cout << "STACK OVERFLOW";
-    }
-    else
-    {
-        stack[top] = new_element;
-        top++;
-        // stack[size] = new_element;
-        // size++;  
-    }
-    // size++;
-   
-    
-    cout << "Dispalying the stack: " << endl;
-    for(i=0; i<size; i++)
-    {
-        printf("%d ", stack[i]);
-    }
-
+    cout << "Enter a number to be pushed into the stack: ";
+    cin >> value;
+    push(stack, value);
+    display();
     return 0;
 }
 
-// CASE 2: Only the top element is displayed for the whole array
-// int main()
-// {
-//     int stack[10], top=-1, MAX=10, new_element;
+int push(int stack[], int value)
+{
+    if(top == MAX - 1)
+    {
+        cout << "Stack overflow!" << endl;
+        top == -1;
+    }
+    else
+    {
+        top++;
+        stack[top] = value;
+        cout << "Value inserted is " << value << endl;
+    }
+}
 
-//     printf("\n ------------ \n");
-//     cout << "Enter the number to be inserted in the stack: ";
-//     cin >> new_element;
-//     cout << endl;
-
-//     cout << "PUSHING ~ INSERTION" << endl;
-//     if (top == MAX-1)
-//     {
-//         cout << "STACK OVERFLOW";
-//     }
-//     else
-//     {
-//         top++;
-//         stack[top] = new_element;
-//     }
-    
-//     cout << "Dispalying the stack: " << endl;
-//     cout << stack[top] << endl;
-    
-//     return 0;
-// }
+void display(void)
+{
+    if (top == -1)
+    {
+        cout << "Stack is empty" << endl;
+    }
+    else
+    {
+        cout << "Stack:" << endl;
+        cout << "[ ";
+        for(i=0; i<MAX; i++)
+        {          
+            cout << stack[i] << " " ;
+        }
+        cout << "]" << endl;
+    }
+}
