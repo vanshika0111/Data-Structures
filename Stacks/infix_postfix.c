622a09d00ff74a166be3addd7db96504fe2625ad
Vanshika <vanshikag0111@gmail.com>
2021-11-05 15:56:02 +0530

    corrected the priority order

--- Stacks/infix_postfix.c
+++ Stacks/infix_postfix.c
@@ -71,6 +71,6 @@ char pop (void)
 int priority(char x)
 {
     if (x == '(') { return 0; }                 // --> marks the priority of '(' as 0
-    if (x == '+' || x == '-') { return 1; }     // --> marks the priority of '+' & '-' as 1
-    if (x == '*' || x == '/') { return 2; }     // --> marks the priority of '*' & '/' as 2
+    if (x == '+' || x == '-') { return 2; }     // --> marks the priority of '+' & '-' as 2
+    if (x == '*' || x == '/') { return 1; }     // --> marks the priority of '*' & '/' as 1
 }
