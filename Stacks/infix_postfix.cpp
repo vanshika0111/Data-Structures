5da66d3679dc2fe1c24c77816d6f6af02017fc10
Vanshika <vanshikag0111@gmail.com>
2021-11-05 15:45:17 +0530

    error of infinite loop to be solved

---
+++ Stacks/infix_postfix.cpp
@@ -0,0 +1,77 @@
+// ---------------- header files----------------
+#include<iostream>
+#include <ctype.h>
+using namespace std;
+
+// ---------------- global declarations---------------- 
+char stack[30];
+int top=-1;
+
+// ---------------- function prototype---------------- 
+void push(char);
+char pop(void);
+int priority(char);
+
+// ---------------- main function---------------- 
+int main()
+{
+    char expression[30], *exp, x;
+    cout << "Enter the expression: ";
+    cin >> expression;
+    cout << endl;
+
+    exp = expression;
+
+    while (*exp != '\0')
+    {
+        if (isalnum(*exp))
+        {
+            cout << *exp;
+        }
+        else if (*exp == '(')
+        {
+            push (*exp);
+        }
+        else if (*exp == ')')
+        {
+            while( (x = pop() ) != '(')
+            {
+                cout << x;
+            }
+        }
+        else
+        {
+            while (priority(stack[top]) >= priority(*exp))
+            {
+                cout << pop();
+            }
+            push(*exp);
+        }   
+        exp++;     
+    }
+    while (top != -1)
+    {
+        cout << pop();
+    }
+
+    return 0;
+}
+
+// ---------------- function definitions---------------- 
+void push (char x)
+{
+    stack[++top] = x;
+}
+
+char pop (void)
+{
+    if (top == -1) { return -1; }
+    else { return stack[top--]; }
+}
+
+int priority(char x)
+{
+    if (x == '(') { return 0; }                 // --> marks the priority of '(' as 0
+    if (x == '+' || x == '-') { return 1; }     // --> marks the priority of '+' & '-' as 1
+    if (x == '*' || x == '/') { return 2; }     // --> marks the priority of '*' & '/' as 2
+}
