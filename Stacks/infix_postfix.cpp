948b54d155d7275f42356a625dc8af7d4f8dfe5a
Vanshika <vanshikag0111@gmail.com>
2021-11-05 15:17:04 +0530

    changed the ext from c to cpp

---
+++ Stacks/infix_postfix.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+// global declarations
+char stack[30];
+int top=-1;
+
+// function prototype
+void push(char);
+char pop(void);
+int priority(char);
+
+// main function
+int main()
+{
+    char expression[30], *expression, x;
+    
+    return 0;
+}
+
+void push (char x)
+{
+    stack[++top] = x;
+}
+
+char pop (void)
+{
+    if (top == -1) { return -1; }
+    else { return stack[top--]; }
+}
+
+int priority(char x)
+{
+    if (x == '(') { return 0; }                 // --> marks the priority of '(' as 0
+    if (x == '+' || x == '-') { return 1; }     // --> marks the priority of '+' & '-' as 1
+    if (x == '*' || x == '/') { return 2; }     // --> marks the priority of '*' & '/' as 2
+}
