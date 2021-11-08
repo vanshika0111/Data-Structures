ab1ddf937796cd3d26bfa74418cedc51bfa95e23
Vanshika <vanshikag0111@gmail.com>
2021-11-08 19:57:31 +0530

    algorithm to convert infix to prefix

--- Stacks/infix_prefix.c
+++ Stacks/infix_prefix.c
@@ -1,12 +1,24 @@
 // program to convert an infix expression into a prefix expression
 
+/*
+Steps to convert an infix expression to prefix:
+    1. Infix expression:                                                  a+b
+    2. Place '(', ')' in the beginning & end of the expression:          (a+b)
+    3. Reverse the string:                                               )b+a(
+    4. Replace "( --> )" & ") --> (":                                    (b+a)
+    5. Convert this infix into a postfix expression:                      ba+
+    6. Reverse the output string which gives a prefix expression:         +ab
+*/
+
+
 // ---------------- header files---------------- 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // ---------------- global declarations---------------- 
 char stack[30];
-int top=-1;
+int top=-1, length;
 
 // ---------------- function prototype---------------- 
 void push(char);
@@ -17,6 +29,11 @@ int priority(char);
 // ---------------- main function---------------- 
 int main()
 {
+    char expression;
+    printf("Enter an infix expression: ");
+    scanf("%s", &expression);
+    length = strlen(expression);
+    printf("\n");
     return 0;
 }
 
@@ -41,5 +58,10 @@ int priority(char x)
 
 void reverse(void)
 {
+    int i;
+    for(i=length; i=0; i--)
+    {
+        printf("%s ", i);
+    }
     
 }
