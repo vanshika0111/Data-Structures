02657e71331053f7be4f7274a24922d77adf9f70
Vanshika <vanshikag0111@gmail.com>
2021-11-08 12:48:53 +0530

    layout for evaluation

--- Stacks/eval_postfix.c
+++ Stacks/eval_postfix.c
@@ -1,1 +1,41 @@
 // program to evaluate a postfix expression
+
+// ---------------- header files---------------- 
+#include<stdio.h>
+#include<ctype.h>
+
+// ---------------- global declarations---------------- 
+char stack[30];
+int top=-1;
+float value;
+
+// ---------------- function prototype---------------- 
+void push(float);
+char pop(void);
+float evaluate(void);
+
+// ---------------- main function---------------- 
+int main()
+{
+    printf("Enter a postfix expression to evaluate: ");
+    scanf("%s", &stack);
+    printf("\n");
+    return 0;
+}
+
+// ---------------- function definitions---------------- 
+void push(float value)
+{
+    stack[++top] = value;
+}
+
+char pop(void)
+{
+    if (top == -1) { return -1; }
+    else { return stack[top--]; }
+}
+
+float evaluate(void)
+{
+
+}
