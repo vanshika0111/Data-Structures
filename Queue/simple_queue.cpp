2c94afbfa871ef66523e1e816f5d95322da29cc7
Vanshika <vanshikag0111@gmail.com>
2021-11-18 20:35:37 +0530

    updated the operations of queue

--- Queue/simple_queue.cpp
+++ Queue/simple_queue.cpp
@@ -1,8 +1,25 @@
 // program to illustrate all the operations of queue in one program
 
+/*
+In simple queue, the following operations take place:
+    1. Enqueue/ insertion from REAR end
+    2. Dequeue/deletion from FRONT end
+    3. Display the simple queue
+*/
+// --------------------------- header files -------------------------
 #include<iostream>
 using namespace std;
+
+// --------------------------- global declaration -------------------------
+
+
+// --------------------------- function prototype -------------------------
+
+
+// --------------------------- main function -------------------------
 int main()
 {
     return 0;
 }
+
+// --------------------------- function definitions -------------------------
